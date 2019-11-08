e3964ddcaf2242e10746c41a195205f1969f414a
Lance McCarthy <lance.mccarthy@progress.com>
2019-11-07 19:02:04 -0500

    Update WeatherWorker.cs
    Changed location key to Boston, made fields readonly.

--- Technical/keynote/Weather/Workers/WeatherWorker.cs
+++ Technical/keynote/Weather/Workers/WeatherWorker.cs
@@ -3,32 +3,28 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using Weather.Model;
-using Weather.Services;
 
 namespace Weather.Workers
 {
     public class WeatherWorker : BackgroundService
     {
-        private ILogger<WeatherWorker> _logger;
-        private IConfiguration _configuration;
-        private IHttpClientFactory _httpClientFactory;
-        private IMemoryCache _cache;
+        private readonly ILogger<WeatherWorker> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IMemoryCache _cache;
 
         public WeatherWorker(ILogger<WeatherWorker> logger,
-                            IConfiguration configuration,
-                            IHttpClientFactory httpClientFactory,
-                            IMemoryCache cache)
+            IConfiguration configuration,
+            IHttpClientFactory httpClientFactory,
+            IMemoryCache cache)
         {
             _logger = logger;
-
             _configuration = configuration;
             _httpClientFactory = httpClientFactory;
             _cache = cache;
@@ -41,14 +37,16 @@ namespace Weather.Workers
                 try
                 {
                     DateTimeOffset? expiresHeader = null;
+
                     var token = _configuration["accuweathertoken"];
+
                     if(!string.IsNullOrWhiteSpace(token))
                     {
                         var client = _httpClientFactory.CreateClient("AccuWeather");
 
-                        var response = await client.GetAsync($"{_configuration["weather:uri"]}/340247?apikey={_configuration["accuweathertoken"]}&details=true");
+                        var response = await client.GetAsync($"{_configuration["weather:uri"]}/348735?apikey={_configuration["accuweathertoken"]}&details=true", stoppingToken);
 
-                        var model = await JsonSerializer.DeserializeAsync<Forecast[]>(await response.Content.ReadAsStreamAsync());
+                        var model = await JsonSerializer.DeserializeAsync<Forecast[]>(await response.Content.ReadAsStreamAsync(), cancellationToken: stoppingToken);
 
                         _cache.Set(Constants.LATEST_FORECAST_CACHE_KEY, model.First());
 
@@ -62,11 +60,11 @@ namespace Weather.Workers
                     //Honoring the expires time
                     if (expiresHeader.HasValue)
                     {
-                       await Task.Delay(expiresHeader.Value.UtcDateTime.Subtract(DateTime.UtcNow));
+                       await Task.Delay(expiresHeader.Value.UtcDateTime.Subtract(DateTime.UtcNow), stoppingToken);
                     }
                     else
                     {
-                        await Task.Delay(TimeSpan.FromMinutes(60));
+                        await Task.Delay(TimeSpan.FromMinutes(60), stoppingToken);
                     }
                 }
                 catch (Exception ex)
